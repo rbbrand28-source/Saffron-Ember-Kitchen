c4835132a9deef331d56f76b8aef99de2ed28153
rbbrand28-source <rbbrand28@gmail.com>
2026-03-15 09:37:11 +0530

    Create page.js

---
+++ app/about/page.js
@@ -0,0 +1,18 @@
+export default function About(){
+
+return(
+
+<div className="p-10 max-w-3xl mx-auto">
+
+<h1 className="text-3xl text-saffron mb-4">
+About Us
+</h1>
+
+<p>
+Saffron Ember Kitchen blends future gastronomy with timeless flavor.
+</p>
+
+</div>
+
+)
+}
