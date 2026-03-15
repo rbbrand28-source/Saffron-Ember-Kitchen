39863181f29d9437b960ef85adfe1271c53f9513
rbbrand28-source <rbbrand28@gmail.com>
2026-03-15 09:42:08 +0530

    Create page.js

---
+++ app/contact/page.js
@@ -0,0 +1,16 @@
+export default function Contact(){
+
+return(
+
+<div className="p-10">
+
+<h1 className="text-3xl text-saffron">
+Contact
+</h1>
+
+<p>Email: contact@saffronember.com</p>
+
+</div>
+
+)
+}
