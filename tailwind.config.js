8ce0d53b54b49f0fa1bcbd0e5f39dfec75a4544c
rbbrand28-source <rbbrand28@gmail.com>
2026-03-15 09:30:30 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./app/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        saffron: "#f59e0b"
+      }
+    }
+  },
+  plugins: []
+}
